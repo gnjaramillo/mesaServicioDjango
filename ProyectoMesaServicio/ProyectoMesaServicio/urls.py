5cba117db7c8feede8f9b3f99fdd8b844a7fba2e
gnjaramillo <gnjaramillo1986@gmail.com>
2024-06-29 22:22:35 -0500

    cambios debido a errores de sincronizacion con drive

--- ProyectoMesaServicio/ProyectoMesaServicio/urls.py
+++ ProyectoMesaServicio/ProyectoMesaServicio/urls.py
@@ -15,29 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from appMesaServicio import views
-
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.inicio),
-    path('login/', views.login),
-    path('inicioAdministrador/', views.inicioAdministrador),
-    path('inicioEmpleado/', views.inicioEmpleado),
-    path('inicioTecnico/', views.inicioTecnico),
-    path('vistaSolicitud/', views.vistaSolicitud),
-    path('registrarSolicitud/', views.registrarSolicitud),
-    path('listarCasosParaAsignar/', views.listarCasosParaAsignar),
-    path('asignarTecnicoCaso/', views.asignarTecnicoCaso),
-    path('salir/', views.salir),
+    path("", include("appMesaServicio.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
-
-
--- ProyectoMesaServicio/appMesaServicio/models.py
+++ ProyectoMesaServicio/appMesaServicio/models.py
@@ -3,139 +3,125 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-tipoOficinaAmbiente=[
-    ('Administrativo','Administrativo'),
-    ('Formación','Formación'),
+tipoOficinaAmbiente = [
+    ('Administrativo', 'Administrativo'),
+    ('Formación', 'Formación')
 ]
 
-tipoUsuario=[
-    ('Administrativo','Administrativo'),
-    ('Instructor','Instructor'),
+tipoUsuario = [
+    ('Administrativo', 'Administrativo'),
+    ('Instructor', 'Instructor')
 ]
 
-estadoCaso = [('Solicitada', 'Solicitada'),
-              ('En proceso', 'En proceso'),
-              ('Finalizada', 'Finalizada'),           
+estadoCaso = [
+    ('Solicitada', 'Solicitada'),
+    ('En Proceso', 'En Proceso'),
+    ('Finalizada', 'Finalizada')
 ]
 
-tipoSolucion = [('Parcial', 'Parcial'),
-                ('Definitiva', 'Definitiva')
+tipoSolucion = [
+    ('Parcial', 'Parcial'),
+    ('Definitiva', 'Definitiva')
 ]
-
-tipoProcedimiento = [('hardware', 'hardware'),
-                ('software', 'software'),
-                ('red', 'red'),
-]
-
-
 
 
 class OficinaAmbiente(models.Model):
     ofiTipo = models.CharField(max_length=15, choices=tipoOficinaAmbiente,
                                db_comment="tipo de oficina")
     ofiNombre = models.CharField(max_length=50, unique=True,
-                               db_comment="nombre de oficina o ambiente")
-    fechaHoraCreacion = models.DateTimeField(auto_now_add=True,
-                               db_comment="fecha y hora del registro")
-    fechaHoraActualizacion = models.DateTimeField(auto_now=True,
-                               db_comment="fecha y hora de ultima actualización")
-    
-    def _str_(self) -> str:
-        return self.ofiNombre
-    
-
-
-
-class User(AbstractUser):
-    userTipo = models.CharField(max_length=15, choices=tipoUsuario,
-                               db_comment="tipo de oficina")
-    userFoto = models.ImageField(upload_to=f"fotos/", null=True, blank=True,
-                               db_comment="foto usuario")
-    fechaHoraCreacion = models.DateTimeField(auto_now_add=True,
-                               db_comment="fecha y hora del registro")
-    fechaHoraActualizacion = models.DateTimeField(auto_now=True,
-                               db_comment="fecha y hora de ultima actualización")
-
-
-    def __str__(self) -> str:
-        return self.username
-    
-
-
-class Solicitud(models.Model):
-    solUsuario = models.ForeignKey(User,on_delete=models.PROTECT,
-                               db_comment="hace ref al empleado que hace la solicitud")
-    solDescripcion = models.TextField(max_length=1000,
-                               db_comment="descripción solicitud del empleado")
-    solOficinaAmbiente = models.ForeignKey(OficinaAmbiente, on_delete=models.PROTECT,
-                               db_comment="hace ref a la oficina o ambiente donde se requiere la solicitud")
-    fechaHoraCreacion = models.DateTimeField(auto_now_add=True,
-                               db_comment="fecha y hora del registro de solicitud")
-    fechaHoraActualizacion = models.DateTimeField(auto_now=True,
-                               db_comment="fecha y hora de ultima actualización de la solicitud")
-
-
-    def _str_(self) -> str:
-        return self.solDescripcion
-    
-
-
-class Caso(models.Model):
-    casSolicitud = models.ForeignKey(Solicitud,on_delete=models.PROTECT,
-                               db_comment="hace ref a la solicitud que genero el caso")
-    casCodigo = models.CharField(max_length=10, unique=True,
-                               db_comment="codigo unico del caso")
-    casUsuario = models.ForeignKey(User,on_delete=models.PROTECT,
-                               db_comment="Empleado de soporte técnico asignado al caso")
-    casEstado = models.CharField(max_length=20, choices=estadoCaso, default='Solicitada' )
-
-    fechaHoraActualizacion = models.DateTimeField(auto_now=True,
-                               db_comment="fecha y hora de ultima actualización de la solicitud")
-
-    def _str_(self) -> str:
-        return self.casCodigo
-    
-
-
-class TipoProcedimiento(models.Model):
-    tipNombre = models.CharField(max_length=30, unique=True,
-                               db_comment="tipo de procedimiento")
-    tipDescripcion = models.TextField(max_length=1000,
-                               db_comment="texto con la descripcion del procedimiento")
+                                 db_comment="Nombre de la oficina o ambiente")
     fechaHoraCreacion = models.DateTimeField(auto_now_add=True,
                                              db_comment="Fecha y hora del registro")
     fechaHoraActualizacion = models.DateTimeField(auto_now=True,
-                               db_comment="fecha y hora de ultima actualización de la solicitud")
-    
+                                                  db_comment="Fecha y hora última actualización")
+
     def __str__(self) -> str:
-            return self.tipNombre
+        return self.ofiNombre
 
 
+class User(AbstractUser):
+    userTipo = models.CharField(
+        max_length=15, choices=tipoUsuario, db_comment="Tipo de usuario")
+    userFoto = models.ImageField(
+        upload_to=f"fotos/", null=True, blank=True, db_comment="Foto del Usuario")
+    fechaHoraCreacion = models.DateTimeField(auto_now_add=True,
+                                             db_comment="Fecha y hora del registro")
+    fechaHoraActualizacion = models.DateTimeField(auto_now=True,
+                                                  db_comment="Fecha y hora última actualización")
 
+    def __str__(self) -> str:
+        return self.username
+
+
+class Solicitud(models.Model):
+    solUsuario = models.ForeignKey(User, on_delete=models.PROTECT,
+                                   db_comment="Hace referencia al empleado que hace la solicitud")
+    solDescripcion = models.TextField(max_length=1000,
+                                      db_comment="Texto que describe las solicitud del empleado")
+    solOficinaAmbiente = models.ForeignKey(
+        OficinaAmbiente, on_delete=models.PROTECT,
+        db_comment="Hace referencia a la oficina o ambiente donde se encuentra el equipo de la solicitud")
+    fechaHoraCreacion = models.DateTimeField(auto_now_add=True,
+                                             db_comment="Fecha y hora del registro")
+    fechaHoraActualizacion = models.DateTimeField(auto_now=True,
+                                                  db_comment="Fecha y hora última actualización")
+
+    def __str__(self) -> str:
+        return self.solDescripcion
+
+
+class Caso(models.Model):
+    casSolicitud = models.ForeignKey(Solicitud, on_delete=models.PROTECT,
+                                     db_comment="Hace referencia a la solicitud que generó el Caso")
+    casCodigo = models.CharField(max_length=20, unique=True,
+                                 db_comment="Código único del caso")
+    casUsuario = models.ForeignKey(User, on_delete=models.PROTECT,
+                                   db_comment="Empleado de soporte técnico asignado al caso")
+    casEstado = models.CharField(
+        max_length=15, choices=estadoCaso, default='Solicitada')
+
+    # fechaHoraCreacion = models.DateTimeField(auto_now_add=True,
+    #                                        db_comment="Fecha y hora del registro")
+    fechaHoraActualizacion = models.DateTimeField(auto_now=True,
+                                                  db_comment="Fecha y hora última actualización")
+
+    def __str__(self) -> str:
+        return f"{self.casCodigo}"
+
+
+class TipoProcedimiento(models.Model):
+    tipNombre = models.CharField(
+        max_length=20, unique=True, db_comment="nombre del tipo de procedimiento")
+    tipDescripcion = models.TextField(max_length=1000, null=True,
+                                      db_comment="Aquí se hace una descripción del tipo de procedimiento")
+
+    fechaHoraCreacion = models.DateTimeField(auto_now_add=True,
+                                             db_comment="Fecha y hora del registro")
+    fechaHoraActualizacion = models.DateTimeField(auto_now=True,
+                                                  db_comment="Fecha y hora última actualización")
+
+    def __str__(self) -> str:
+        return self.tipNombre
 
 
 class SolucionCaso(models.Model):
     solCaso = models.ForeignKey(Caso, on_delete=models.PROTECT,
-                               db_comment="hace ref a la solicitud que genero el caso")   
-
+                                db_comment="Hace referencia al caso que genera la solución")
     solProcedimiento = models.TextField(max_length=2000,
                                         db_comment="Texto del procedimiento realizado en la solución del caso")
     solTipoSolucion = models.CharField(max_length=20, choices=tipoSolucion,
                                        db_comment="Tipo de la solucuín, si es parcial o definitiva")
     fechaHoraCreacion = models.DateTimeField(auto_now_add=True,
-                                             db_comment="Fecha y hora de creacion")
+                                             db_comment="Fecha y hora del registro")
     fechaHoraActualizacion = models.DateTimeField(auto_now=True,
-                                                    db_comment="Fecha y hora de actualizacion")
-    def __str__(self):
-        return self.solTipoSolucion   
+                                                  db_comment="Fecha y hora última actualización")
+
+    def __str__(self) -> str:
+        return self.solTipoSolucion
 
 
-
-
-class SolucionCasotipoProcedimiento (models.Model):
-    solSolucionCaso =models.ForeignKey(SolucionCaso, on_delete=models.PROTECT, db_comment="Llave foranea de Solucion del caso")
-    solTipoProcedimiento =models.ForeignKey(TipoProcedimiento, on_delete=models.PROTECT, db_comment="Llave foranea de Solucion de Tipo de procedimiento")
-    def __str__(self):
-        return f"{self.sp_solucion_caso},{self.sp_tipo_procedimiento}"
-
-
+class SolucionCasoTipoProcedimientos(models.Model):
+    solSolucionCaso = models.ForeignKey(SolucionCaso, on_delete=models.PROTECT,
+                                        db_comment="Hace referencia a la solución del Caso")
+    solTipoProcedimiento = models.ForeignKey(TipoProcedimiento, on_delete=models.PROTECT,
+                                             db_comment="Hace referencia al tipo de procedimiento de la solución")
